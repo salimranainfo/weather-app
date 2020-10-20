6cf8d6b63c547fb33dd29ab7f3005a23e083d832
ranasaliminfo <rana.salim.info@gmail.com>
2020-10-20 06:23:08 +0600

    finished app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div class="container">
+      <h1>Weather App</h1>
+      <div>
+        <h2>Todays Weather Data</h2>
+        <div>
+          <p>Location: {{ location }}</p>
+          <ul v-if="apiData">
+            <li>Temp: {{ (apiData.temp - 273.15).toFixed(2) }}&#8451;</li>
+            <li>
+              Max Temp: {{ (apiData.temp_max - 273.15).toFixed(2) }}&#8451;
+            </li>
+            <li>
+              Min Temp: {{ (apiData.temp_min - 273.15).toFixed(2) }}&#8451;
+            </li>
+            <li>
+              Feels Like: {{ (apiData.feels_like - 273.15).toFixed(2) }}&#8451;
+            </li>
+            <li>Humidity: {{ apiData.humidity }}%</li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      apiData: null,
+      location: ''
+    }
+  },
+  created() {
+    axios
+      .get(
+        'http://api.openweathermap.org/data/2.5/weather?q=Dhaka,BD&APPID=41177bba6c535be0bb4500b47ff24769'
+      )
+      .then(res => {
+        const location = res.data.name
+        const data = res.data.main
 
-#nav {
-  padding: 30px;
-}
+        this.location = location
+        this.apiData = data
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+        console.log(res.data)
+      })
+  }
 }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style scopped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+.container {
+  max-width: 50%;
+  border: 2px solid gray;
+  border-radius: 10px;
+  margin: 0 auto;
+  padding: 20px;
 }
 </style>
